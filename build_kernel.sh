ec8d9ba6d8726f6a7471b20971fe7b493e370750
Fede2782 <78815152+Fede2782@users.noreply.github.com>
2024-01-23 21:03:49 +0100

    build_script: add missing variables

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,7 +5,9 @@ export CROSS_COMPILE=$(pwd)/toolchain/clang/host/linux-x86/clang-r383902/bin/aar
 export CC=$(pwd)/toolchain/clang/host/linux-x86/clang-r383902/bin/clang
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export ARCH=arm64
-export PLATFORM_VERSION=12
+export PLATFORM_VERSION=14
+export TARGET_SOC=mt6877
+export TARGET_BUILD_VARIANT=user
 
 export KCFLAGS=-w
 export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
